1efee59a46a8715a1f9ae95227472d90b0da6cf8
grifth <2473713213@qq.com>
2018-12-10 23:13:36 +0800

    fuck code

--- main.js
+++ main.js
@@ -1,25 +1,62 @@
-class VNode{
-  constructor(tag,children,text){
-    this.tag = tag
-    this.text = text
-    this.children = children
-  }
-  render(){
-    if(this.tag === '#text'){
-      return document.createTextNode(this.text)
-    }
-    let el = document.createElement(this.tag)
-    this.children.forEach(vChil=>{
-      el.appendChild(vChild.render())
-    })
-    return el
-  }
-}
-
-function v(tag,children,text){
-  if(typeof children ==='string'){
-    text = children
+function Element (tagName, props, children) {
+  this.tagName = tagName
+  this.props = props
+  if(children===undefined){
     children = []
   }
-  return new VNode(tag,children,text)
+  this.children = children
+}
+
+Element.prototype.render = function () {
+  var el = document.createElement(this.tagName) // 根据tagName构建
+  var props = this.props
+
+  for (var propName in props) { // 设置节点的DOM属性
+    var propValue = props[propName]
+    el.setAttribute(propName, propValue)
+  }
+
+  // var children = this.children || []
+  var children = this.children
+  children.forEach(function(child) {
+    var childEl = (child instanceof Element)
+      ? child.render() // 如果子节点也是虚拟DOM，递归构建DOM节点
+      : document.createTextNode(child) // 如果字符串，只构建文本节点
+    el.appendChild(childEl)
+  })
+
+  return el
+
+}
+
+function cel(tagName, props, children){
+  let temp = new Element(tagName, props, children)
+  return temp
+}
+
+function diff (oldTree, newTree) {
+  var index = 0 // 当前节点的标志
+  var patches = {} // 用来记录每个节点差异的对象
+  dfsWalk(oldTree, newTree, index, patches)
+  return patches
+}
+function dfsWalk (oldNode, newNode, index, patches) {
+  // 对比oldNode和newNode的不同，记录下来
+  patches[index] = []
+
+  diffChildren(oldNode.children, newNode.children, index, patches)
+}
+
+// 遍历子节点
+function diffChildren (oldChildren, newChildren, index, patches) {
+  var leftNode = null
+  var currentNodeIndex = index
+  oldChildren.forEach(function (child, i) {
+    var newChild = newChildren[i]
+    currentNodeIndex = (leftNode && leftNode.count) // 计算节点的标识
+      ? currentNodeIndex + leftNode.count + 1
+      : currentNodeIndex + 1
+    dfsWalk(child, newChild, currentNodeIndex, patches) // 深度遍历点
+    leftNode = child
+  })
 }
